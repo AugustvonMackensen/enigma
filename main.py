7be9b97fbc3224f98deb947d734ceebf7f3710a4
Leo Lee <bonaparte_19th@hotmail.com>
2022-12-24 14:19:49 +0900

    Enigma Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Reflector : A
+Rotors : I- II - III
+Plugboard : A-R, G-K, O-X
+Message : A => X
+"""
+
+from Keyboard import Keyboard
+from Plugboard import Plugboard
+from Rotor import Rotor
